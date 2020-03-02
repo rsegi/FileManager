778be265bef420a4559d7bf8ff049c91a723c0c8
Raúl Seguí <r.segui.t@gmail.com>
2020-03-02 18:59:46 +0100

    Add full CRUD implementation. close #11

--- FileManager.Presentation.WinSite/frmStudent.cs
+++ FileManager.Presentation.WinSite/frmStudent.cs
@@ -14,13 +14,13 @@ namespace FileManager.Presentation.WinSite
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            
+
             Student student = new Student(int.Parse(txtBoxStudentId.Text), txtBoxName.Text, txtBoxSurname.Text, DateTime.Parse(txtBoxBirthDate.Text));
             string choice = "VuelingFile";
-            
+
             var factory = FactoryProvider.getFactory(choice);
-            var miFactoria = factory.Create((ComboBox.SelectedItem.ToString()));
-            miFactoria.Add(student);
+            var fileFactory = factory.Create((ComboBox.SelectedItem.ToString()));
+            fileFactory.Add(student);
 
 
         }
@@ -28,66 +28,28 @@ namespace FileManager.Presentation.WinSite
         private void BtnDelete_Click(object sender, EventArgs e)
         {
             Student student = new Student(int.Parse(txtBoxStudentId.Text), txtBoxName.Text, txtBoxSurname.Text, DateTime.Parse(txtBoxBirthDate.Text));
-            switch (ComboBox.SelectedItem)
-            {
-                case "txt":
-                    TxtFile txtFileFactory = new TxtFile();
-                    txtFileFactory.Remove(student);
-                    break;
-                case "json":
-                    JsonFile jsonFileFactory = new JsonFile();
-                    jsonFileFactory.Remove(student);
-                    break;
-                case "xml":
-                    XmlFile xmlFileFactory = new XmlFile();
-                    xmlFileFactory.Remove(student);
-                    break;
-                default:
-                    break;
-            }
+            string choice = "VuelingFile";
+            var factory = FactoryProvider.getFactory(choice);
+            var fileFactory = factory.Create((ComboBox.SelectedItem.ToString()));
+            fileFactory.Remove(student);
         }
+
 
         private void BtnUpdate_Click(object sender, EventArgs e)
         {
             Student student = new Student(int.Parse(txtBoxStudentId.Text), txtBoxName.Text, txtBoxSurname.Text, DateTime.Parse(txtBoxBirthDate.Text));
-            switch (ComboBox.SelectedItem)
-            {
-                case "txt":
-                    TxtFile txtFileFactory = new TxtFile();
-                    txtFileFactory.Update(student);
-                    break;
-                case "json":
-                    JsonFile jsonFileFactory = new JsonFile();
-                    jsonFileFactory.Update(student);
-                    break;
-                case "xml":
-                    XmlFile xmlFileFactory = new XmlFile();
-                    xmlFileFactory.Update(student);
-                    break;
-                default:
-                    break;
-            }
+            string choice = "VuelingFile";
+            var factory = FactoryProvider.getFactory(choice);
+            var fileFactory = factory.Create((ComboBox.SelectedItem.ToString()));
+            fileFactory.Update(student);
         }
 
         private void BtnList_Click(object sender, EventArgs e)
         {
-            switch (ComboBox.SelectedItem)
-            {
-                case "txt":
-                    TxtFile txtFileFactory = new TxtFile();
-                    MessageBox.Show(txtFileFactory.List());
-                    break;
-                case "json":
-                    JsonFile jsonFileFactory = new JsonFile();
-                    MessageBox.Show(jsonFileFactory.List());
-                    break;
-                case "xml":
-                    XmlFile xmlFileFactory = new XmlFile();
-                    MessageBox.Show(xmlFileFactory.List());
-                    break;
-                default:
-                    break;
-            }
+            string choice = "VuelingFile";
+            var factory = FactoryProvider.getFactory(choice);
+            var fileFactory = factory.Create((ComboBox.SelectedItem.ToString()));
+            MessageBox.Show(fileFactory.List());
         }
     }
 }
