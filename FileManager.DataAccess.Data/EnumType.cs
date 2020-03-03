377c6db097b415750b1065875d396da907f17072
rsegt <r.segui.t@gmail.com>
2020-03-03 19:41:53 +0100

    Create class XmlUtil and delete unused class. close #17

--- FileManager.DataAccess.Data/EnumType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileManager.DataAccess.Data
-{
-    public enum EnumType
-    {
-        txt = 1,
-        json = 2,
-        xml = 3
-    }
-}
---
+++ FileManager.DataAccess.Data/XmlUtils.cs
@@ -0,0 +1,52 @@
+﻿using FileManager.Common.Layer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FileManager.DataAccess.Data
+{
+    class XmlUtils
+    {
+        readonly string path = ConfigurationManager.AppSettings["xmlPath"];
+        private bool FileExists()
+        {
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            else
+            {
+                CreateFile();
+                return false;
+            }
+        }
+
+        private bool CreateFile()
+        {
+            XDocument doc = new XDocument(new XElement("Students"));
+            doc.Save(path);
+            return true;
+        }
+
+        public Student AddStudent(Student student)
+        {
+            XDocument doc = XDocument.Load(path);
+            XElement students = doc.Element("Students");
+            students.Add(new XElement("Student",
+                           new XElement("StudentId", student.StudentId),
+                           new XElement("Name", student.Name),
+                           new XElement("Surname", student.Surname),
+                           new XElement("BirthDate", student.BirthDate)));
+            doc.Save(path);
+            return student;
+        }
+
+
+
+    }
+}
