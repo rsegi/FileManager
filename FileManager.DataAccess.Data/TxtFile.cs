022c2789b59bd0aed2106adfc5bf2956bccc05d4
rsegt <r.segui.t@gmail.com>
2020-03-03 19:38:51 +0100

    Refactor clas TxtFile. close #14

--- FileManager.DataAccess.Data/TxtFile.cs
+++ FileManager.DataAccess.Data/TxtFile.cs
@@ -1,121 +1,37 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Configuration;
-using FileManager.Common.Layer;
-using System.Collections.Generic;
+﻿using FileManager.Common.Layer;
 
 namespace FileManager.DataAccess.Data
 {
-    
     public class TxtFile : VuelingFile
     {
+        readonly TxtUtils utils = new TxtUtils();
 
-        readonly string path = ConfigurationManager.AppSettings["txtPath"];
         public override Student Add(Student student)
         {
-            var writer = new StringBuilder();
-
-            writer.AppendLine($"{student.StudentId},{student.Name},{student.Surname},{student.BirthDate}");
-
-            File.AppendAllText(path, writer.ToString());
-
-            return student;
+            utils.FileExists();
+            var studentAdded = utils.AddStudent(student);
+            return studentAdded;
         }
 
         public override string List()
         {
-            if (File.Exists(path))
-            {
-                var writer = new StringBuilder();
-                using (var reader = new StreamReader(path))
-                {
-                    while (!reader.EndOfStream)
-                    {
-                        var line = reader.ReadLine();
-
-                        writer.Append(line + "\n");
-                    }
-                }
-                string message = writer.ToString();
-                return message;
-            }
-            return null;
-            
+            utils.FileExists();
+            var list = utils.List();
+            return list;           
         }
 
         public override Student Remove(Student student)
         {
-            if (File.Exists(path))
-            {
-                var writer = new StringBuilder();
-                var studentsList = new List<Student>();
-                using (var reader = new StreamReader(path))
-                {
-                    while (!reader.EndOfStream)
-                    {
-                        var studentInTxt = new Student();
-                        var line = reader.ReadLine();
-                        var values = line.Split(',');
-
-                        studentInTxt.StudentId = int.Parse(values[0]);
-                        studentInTxt.Name = values[1];
-                        studentInTxt.Surname = values[2];
-                        studentInTxt.BirthDate = DateTime.Parse(values[3]);
-
-                        studentsList.Add(studentInTxt);
-                    }
-                }
-                Student studentToErase = studentsList.Find(x => x.StudentId == student.StudentId);
-                studentsList.Remove(studentToErase);
-
-                foreach (var element in studentsList)
-                {
-                    writer.AppendLine($"{element.StudentId},{element.Name},{element.Surname},{element.BirthDate}");
-
-                    File.WriteAllText(path, writer.ToString());
-                }
-                return student;
-            }
-            return null;
+            utils.FileExists();
+            var removedStudent = utils.RemoveStudent(student);
+            return removedStudent;
         }
 
         public override Student Update(Student student)
         {
-            if (File.Exists(path))
-            {
-                var writer = new StringBuilder();
-                var studentsList = new List<Student>();
-                using (var reader = new StreamReader(path))
-                {
-                    while (!reader.EndOfStream)
-                    {
-                        var studentInTxt = new Student();
-                        var line = reader.ReadLine();
-                        var values = line.Split(',');
-
-                        studentInTxt.StudentId = int.Parse(values[0]);
-                        studentInTxt.Name = values[1];
-                        studentInTxt.Surname = values[2];
-                        studentInTxt.BirthDate = DateTime.Parse(values[3]);
-
-                        studentsList.Add(studentInTxt);
-                    }
-                }
-                Student studentToUpdate = studentsList.Find(x => x.StudentId == student.StudentId);
-                studentsList.Remove(studentToUpdate);
-
-                studentsList.Add(student);
-
-                foreach (var element in studentsList)
-                {
-                    writer.AppendLine($"{element.StudentId},{element.Name},{element.Surname},{element.BirthDate}");
-
-                    File.WriteAllText(path, writer.ToString());
-                }
-                return student;
-            }
-            return null;
+            utils.FileExists();
+            var updatedStudent = utils.UpdateStudent(student);
+            return updatedStudent;
         }
     }
 }
